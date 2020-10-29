ecd3a367ecfea926afb90c708a7c6346476a793a
Supriya G <sgadkar1@my.centennialcollege.ca>
2020-10-29 14:03:45 -0500

    Initial commit

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+ 
+    @IBOutlet var firstItemStepper: UIStepper!
+    @IBOutlet var firstItemQuantity: UILabel!
+    
+    @IBOutlet var seconItemStepper: UIStepper!
+    @IBOutlet var secondItemQuantity: UILabel!
+    
+    @IBOutlet var thirdItemStepper: UIStepper!
+    @IBOutlet var thirdItemQuantity: UILabel!
+    
+    @IBOutlet var fourItemStepper: UIStepper!
+    @IBOutlet var fourthItemQuantity: UILabel!
+    
+    @IBOutlet var fifthItemStepper: UIStepper!
+    @IBOutlet var fifthItemQuantity: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       // firstItemStepper.autorepeat = true
+        //firstItemStepper.isContinuous = true
+        //firstItemQuantity.text = firstItemStepper.value.description
     }
-
-
+    
+    @IBAction func firstStepperPressed(_ sender: UIStepper) {
+        firstItemQuantity.text = Int(firstItemStepper.value).description
+        
+        secondItemQuantity.text = Int(seconItemStepper.value).description
+        
+        thirdItemQuantity.text = Int(thirdItemStepper.value).description
+        
+        fourthItemQuantity.text = Int(fourItemStepper.value).description
+        
+        fifthItemQuantity.text = Int(fifthItemStepper.value).description
+    }
+    
+    
 }
 
